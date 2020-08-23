25b951fb50576e155fcd409b15ef958f55cfebf8
mq <>
2020-08-23 10:25:12 +0200

    fix debug bool

--- django-twitch-archive/settings.py
+++ django-twitch-archive/settings.py
@@ -30,7 +30,7 @@ env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = list(env("ALLOWED_HOSTS").split(","))
 
