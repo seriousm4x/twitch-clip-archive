6c97e0bd137dd0203801d2b20159924bd9fc93c1
seriousm4x <maxi@quoss.org>
2020-09-17 19:05:39 +0200

    fix debug env

--- django-twitch-archive/settings.py
+++ django-twitch-archive/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DJANGO_DEBUG')
+DEBUG = bool(os.getenv('DJANGO_DEBUG'))
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(",")
 
