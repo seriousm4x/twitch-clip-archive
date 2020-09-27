4a5fabbc8b4209cc6790edbb6560e5dfc7cfbf5f
seriousm4x <maxi@quoss.org>
2020-09-27 03:12:24 +0200

    show unknown category for categories deleted on twitch

--- clips/utils.py
+++ clips/utils.py
@@ -6,13 +6,15 @@ import clips
 
 def matchGameToClip(cliplist):
     if type(cliplist) is clips.models.Clip:
-        cliplist.game_title = Game.objects.filter(
-            game_id=cliplist.game_id).get().name
-        cliplist.game_thumbnail = Game.objects.filter(
-            game_id=cliplist.game_id).get().box_art_url
+        if cliplist.game_id == None:
+            cliplist.game_title = "Unknown category"
+        else:
+            cliplist.game_title = Game.objects.filter(
+                game_id=cliplist.game_id).get().name
     else:
         for clip in cliplist:
-            clip.game_title = Game.objects.filter(
-                game_id=clip.game_id).get().name
-            clip.game_thumbnail = Game.objects.filter(
-                game_id=clip.game_id).get().box_art_url
+            if clip.game_id == None:
+                clip.game_title = "Unknown category"
+            else:
+                clip.game_title = Game.objects.filter(
+                    game_id=clip.game_id).get().name
