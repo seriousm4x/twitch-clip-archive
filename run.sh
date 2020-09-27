0b030030412ffb81648fe0a76c92edda17f7e3bd
seriousm4x <maxi@quoss.org>
2020-09-27 11:08:35 +0200

    run python commands unbuffered

--- run.sh
+++ run.sh
@@ -6,20 +6,20 @@ if [ "$USE_POSTGRESQL" == "True" ]; then
 fi
 
 # DB migration
-python manage.py makemigrations
-python manage.py migrate
+python -u manage.py makemigrations
+python -u manage.py migrate
 
 # Import data from sqlite
 if [ -f "datadump.json" ]; then
-    python manage.py loaddata datadump.json
+    python -u manage.py loaddata datadump.json
     rm datadump.json
 fi
 
 # Create superuser if none exists
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('$DJANGO_SUPERUSER_USER', password='$DJANGO_SUPERUSER_PASSWORD') if not User.objects.filter(username='$DJANGO_SUPERUSER_USER').exists() else print('Django user exists')"
+python -u manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('$DJANGO_SUPERUSER_USER', password='$DJANGO_SUPERUSER_PASSWORD') if not User.objects.filter(username='$DJANGO_SUPERUSER_USER').exists() else print('Django user exists')"
 
 # Create twitch settings if none exists
-python manage.py shell -c "from clips.models import TwitchSettings; TwitchSettings(broadcaster_name='$TWITCH_CHANNEL', client_id='$TWITCH_CLIENT_ID', client_secret='$TWITCH_CLIENT_SECRET').save() if TwitchSettings.objects.filter().count() == 0 else print('Twitch settings exists')"
+python -u manage.py shell -c "from clips.models import TwitchSettings; TwitchSettings(broadcaster_name='$TWITCH_CHANNEL', client_id='$TWITCH_CLIENT_ID', client_secret='$TWITCH_CLIENT_SECRET').save() if TwitchSettings.objects.filter().count() == 0 else print('Twitch settings exists')"
 
 # Run server
 gunicorn django-twitch-archive.wsgi:application --bind 0.0.0.0:8000 --workers $(($(nproc) + 1)) &
@@ -34,8 +34,8 @@ printf "\n\n"
 
 # Download from twitch
 while :; do
-    python manage.py updateDB && \
-    python manage.py download && \
-    python manage.py collectstatic --noinput
+    python -u manage.py updateDB && \
+    python -u manage.py download && \
+    python -u manage.py collectstatic --noinput
     sleep $(( 6 * 60 * 60 )) # 6 hours
 done
