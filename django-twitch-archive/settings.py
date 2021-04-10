1fbf7a5eefa05878c9a323d99f02090145c7cb16
Maxi Quoß <maxi@quoss.org>
2021-04-10 12:37:41 +0200

    define default_auto_field for django 3.2

--- django-twitch-archive/settings.py
+++ django-twitch-archive/settings.py
@@ -98,6 +98,8 @@ else:
         }
     }
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
