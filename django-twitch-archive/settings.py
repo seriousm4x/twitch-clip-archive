39ee13ba12cd0fb28804515110164ddd8e418b1f
Maxi Quoß <maxi@quoss.org>
2023-02-01 19:25:59 +0100

    load .env file if available

--- django-twitch-archive/settings.py
+++ django-twitch-archive/settings.py
@@ -12,10 +12,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+
+# load .env file
+if os.path.isfile(BASE_DIR / ".env"):
+    load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
